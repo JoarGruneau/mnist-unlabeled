85e82d547197e966521e961d943010208603aaa1
Joar Gruneau <joar@gruneau.se>
2020-07-11 17:30:00 +0000

    cleaned up dataset

--- data.py
+++ data.py
@@ -35,30 +35,12 @@ clean_transform = transforms.Compose(
 
 
 def get_data_generators(batch_size):
-    train = datasets.MNIST(root='./data',
-                           train=True,
-                           download=True,
-                           transform=clean_transform)
+    train = datasets.MNIST(root='./data', train=True, download=True)
+    train_set = Dataset(train, clean_transform, augumentation_transform)
+    labeled_data = get_labeled_data(train, clean_transform)
 
-    train_aug = datasets.MNIST(root='./data',
-                               train=True,
-                               download=True,
-                               transform=augumentation_transform)
-
-    train_set = Dataset(train, train_aug)
-    labeled_data = get_labeled_data(train)
-
-    test = datasets.MNIST(root='./data',
-                          train=False,
-                          download=True,
-                          transform=clean_transform)
-
-    test_aug = datasets.MNIST(root='./data',
-                              train=False,
-                              download=True,
-                              transform=augumentation_transform)
-
-    test_set = Dataset(test, test_aug)
+    test = datasets.MNIST(root='./data', train=False, download=True)
+    test_set = Dataset(test, clean_transform, augumentation_transform)
 
     train_generator = torch.utils.data.DataLoader(train_set,
                                                   shuffle=True,
@@ -68,31 +50,32 @@ def get_data_generators(batch_size):
     test_generator = torch.utils.data.DataLoader(test_set,
                                                  shuffle=False,
                                                  num_workers=8,
-                                                 batch_size=int(len(test)/10))
+                                                 batch_size=int(
+                                                     len(test) / 10))
 
     return labeled_data, train_generator, test_generator
 
 
-def get_labeled_data(data):
+def get_labeled_data(data, transform):
     labels = []
     labeled_data = []
     for i in range(len(data)):
         if data[i][1] not in labels:
-            labeled_data.append(data[i][0])
+            labeled_data.append(transform(data[i][0]))
             labels.append(data[i][1])
         if len(labels) == 10:
             return torch.stack(labeled_data, dim=0), torch.tensor(labels)
 
 
 class Dataset(torch.utils.data.Dataset):
-    def __init__(self, data, aug_data):
+    def __init__(self, data, clean_transform, aug_transform):
         self.data = data
-        self.aug_data = aug_data
+        self.clean_transform = clean_transform
+        self.aug_transform = augumentation_transform
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
         x, y = self.data[idx]
-        x_aug, _ = self.aug_data[idx]
-        return (x, x_aug), y
+        return (self.clean_transform(x), self.aug_transform(x)), y
